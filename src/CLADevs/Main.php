fe7a3747c3c9bd2266f69e67f368ccbbc92fd340
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-13 19:21:14 +0100

    Update Main.php

--- src/CLADevs/Main.php
+++ src/CLADevs/Main.php
@@ -12,10 +12,9 @@ use pocketmine\utils\TextFormat as CLADevs;
 class Main extends PluginBase implements Listener {
   
   const PREIX = "§fCLA§cDevs §7>§r ";
-  const AUTHOR = "CLADevs";
   
   public function onEnable(){
-    $this->getLogger()->info(self::PREFIX . CLLADevs::GREEN . "Enabled. Build by " . CLADevs::RED . self::AUTHOR);
+    $this->getLogger()->info(self::PREFIX . CLLADevs::GREEN . "Enabled. Build by CLADevs");
     
   }
   
@@ -23,7 +22,6 @@ class Main extends PluginBase implements Listener {
    $player = $event->getPlayer();
     $name = $player->getName();
     
-        $event->setJoinMessage("§a$name §ehas §eJoining he Server");
         $player->setFood(20);
         $player->setHealth(20);
         $player->setGamemode(0);
